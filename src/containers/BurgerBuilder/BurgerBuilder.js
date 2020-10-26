46887e99a43ea85f1fbe8e06ac8702cf1c602d2f
swapnil042 <sheikhswapnil.musa@gmail.com>
2020-10-27 01:19:00 +0600

    Implemented Order Nav

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -113,7 +113,8 @@ class BurgerBuilder extends Component{
         for(let i in this.state.ingredients){
             queryParams.push(encodeURIComponent(i)+ '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-
+        
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&');
 
         this.props.history.push({
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component{
     state={
@@ -9,16 +11,23 @@ class Checkout extends Component{
             meat: 1,
             bacon: 1,
             cheese: 1
-        }
+        },
+        price: 0
     }
 
     componentDidMount(){
-        const query = new URLSearchParams(this.props.location.search);
-        const ingredients = {}
-        for(let params of query.entries()){
-            ingredients[params[0]] = +params[1];
+        const query = new URLSearchParams( this.props.location.search );
+        const ingredients = {};
+        let price = 0;
+        for ( let param of query.entries() ) {
+            // ['salad', '1']
+            if (param[0] === 'price') {
+                price = param[1];
+            } else {
+                ingredients[param[0]] = +param[1];
+            }
         }
-        this.setState({ingredients});
+        this.setState( { ingredients: ingredients, totalPrice: price } );
     }
 
     checkoutCancelHandler = () =>{
@@ -29,10 +38,15 @@ class Checkout extends Component{
     }
     render(){
         return(
-            <CheckoutSummary 
+            <div>
+                <CheckoutSummary 
                 ingredients={this.state.ingredients}
                 checkoutCanceled={this.checkoutCancelHandler}
                 checkoutContinued={this.checkoutContinueHandler}/>
+            <Route 
+                path={this.props.match.path + '/contact-data'} 
+                render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />)} />
+            </div>
         );
         
     }
